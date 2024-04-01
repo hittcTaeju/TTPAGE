bc530d2ed6e2996e5ea0a2232067611f05545dc3
taeju <taejoo0112@naver.com>
2024-04-01 15:23:06 +0900

    게시물 id로 url 까진했음...

--- src/main/java/TAEJU/TTPAGE/controller/BoardController.java
+++ src/main/java/TAEJU/TTPAGE/controller/BoardController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -46,6 +47,10 @@ public class BoardController {
         List<Post> posts = postService.findPosts();
         model.addAttribute("posts",posts);
         return "board/boardList";
+    }
+    @GetMapping("/board/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id){
+       return null;
     }
 
 
